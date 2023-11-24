6945f93228f8bcab977d816645d78527e2b0994a
Mohammad Alikhani <dev.alikhani@gmail.com>
2023-11-24 12:39:40 +0330

    fix

--- install.sh
+++ install.sh
@@ -3,41 +3,30 @@
 # Function to install Caddy
 install_caddy() {
     echo "Installing Caddy..."
-    sudo apt-get install -y debian-keyring debian-archive-keyring apt-transport-https
-    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/caddy-stable-archive-keyring.gpg] https://dl.cloudsmith.io/public/caddy/stable/deb/debian any-version main" | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-    sudo apt-get update
-    if sudo apt-get install caddy; then
-        echo "Caddy installed successfully."
-    else
-        echo "Failed to install Caddy."
-        exit 1
-    fi
+    apt-get install -y debian-keyring debian-archive-keyring apt-transport-https
+    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/caddy-stable-archive-keyring.gpg] https://dl.cloudsmith.io/public/caddy/stable/deb/debian any-version main" | tee /etc/apt/sources.list.d/caddy-stable.list
+    apt-get update
+    apt-get install caddy
 }
 
 # Function to install HAProxy
 install_haproxy() {
     echo "Installing HAProxy..."
-    sudo apt-get update
-    if sudo apt-get install haproxy; then
-        echo "HAProxy installed successfully."
-    else
-        echo "Failed to install HAProxy."
-        exit 1
-    fi
+    apt-get update
+    apt-get install haproxy
 }
 
-# Function to configure Caddy and HAProxy
-configure_caddy_haproxy() {
-    # Configure Caddy
-    echo "Configuring Caddy for $domain..."
-    cat <<EOF | sudo tee /etc/caddy/Caddyfile
+# Function to configure Caddy
+configure_caddy() {
+    echo "Configuring Caddy for $caddy_domain..."
+    cat <<EOF | tee /etc/caddy/Caddyfile
 {
     http_port 8080
     https_port $caddy_port
 }
 
-$domain {
+$caddy_domain {
     handle /sub/* {
         root * /var/www/textfiles/
         file_server
@@ -53,11 +42,13 @@ $domain {
 EOF
 
     # Restart Caddy to apply changes
-    sudo systemctl restart caddy
+    systemctl restart caddy
+}
 
-    # Configure HAProxy
+# Function to configure HAProxy
+configure_haproxy() {
     echo "Configuring HAProxy..."
-    cat <<EOF | sudo tee /etc/haproxy/haproxy.cfg
+    cat <<EOF | tee /etc/haproxy/haproxy.cfg
 global
     log /dev/log local0
     log /dev/log local1 notice
@@ -81,15 +72,20 @@ frontend https_in
     mode tcp
     tcp-request inspect-delay 5s
     tcp-request content accept if { req_ssl_hello_type 1 }
-    use_backend caddy_backend if { req_ssl_sni -i $domain }
+    use_backend caddy_backend if { req_ssl_sni -i $caddy_domain }
+    use_backend default_service_backend if { req_ssl_sni -i $default_sni_domain }
 
 backend caddy_backend
     mode tcp
-    server caddy_server 127.0.0.1:5003 check
+    server caddy_server 127.0.0.1:$caddy_port check
+
+backend default_service_backend
+    mode tcp
+    server default_service 127.0.0.1:$default_service_port check
 EOF
 
     # Restart HAProxy to apply changes
-    sudo systemctl restart haproxy
+    systemctl restart haproxy
 }
 
 # Function to add a new service to HAProxy
@@ -102,13 +98,13 @@ add_service_to_haproxy() {
     echo "
 backend new_service_backend
     mode tcp
-    server new_service 127.0.0.1:$service_port check" | sudo tee -a /etc/haproxy/haproxy.cfg
+    server new_service 127.0.0.1:$service_port check" | tee -a /etc/haproxy/haproxy.cfg
 
     # Modify the frontend to add a rule for the new service
-    sudo sed -i "/frontend https_in/a \    use_backend new_service_backend if { req_ssl_sni -i $service_sni }" /etc/haproxy/haproxy.cfg
+    sed -i "/frontend https_in/a \    use_backend new_service_backend if { req_ssl_sni -i $service_sni }" /etc/haproxy/haproxy.cfg
 
     # Restart HAProxy to apply changes
-    sudo systemctl restart haproxy
+    systemctl restart haproxy
 
     echo "New service added to HAProxy."
 }
@@ -118,25 +114,25 @@ echo "1. Install and configure Caddy and HAProxy from scratch."
 echo "2. Add a new service to an existing HAProxy setup."
 read -p "Choose an option (1 or 2): " choice
 
-case "$choice" in
-    1)
-        read -p "Enter the domain to be served by Caddy: " domain
-        read -p "Enter the port for Caddy to listen on (e.g., 5003): " caddy_port
-        if ! command -v caddy &> /dev/null || ! command -v haproxy &> /dev/null; then
-            install_caddy
-            install_haproxy
-        fi
-        configure_caddy_haproxy
-        ;;
-    2)
-        if ! command -v haproxy &> /dev/null; then
-            echo "HAProxy is not installed. Installing now."
-            install_haproxy
-        fi
-        add_service_to_haproxy
-        ;;
-    *)
-        echo "Invalid choice. Exiting."
-        exit 1
-        ;;
-esac
+if [ "$choice" == "1" ]; then
+    read -p "Enter the domain to be served by Caddy: " caddy_domain
+    read -p "Enter the port for Caddy to listen on (e.g., 5003): " caddy_port
+    read -p "Enter the SNI domain for the default service (e.g., default.example.com): " default_sni_domain
+    read -p "Enter the port for the default service (e.g., 5004): " default_service_port
+
+    if ! command -v caddy &> /dev/null || ! command -v haproxy &> /dev/null; then
+        install_caddy
+        install_haproxy
+    fi
+    configure_caddy
+    configure_haproxy
+elif [ "$choice" == "2" ]; then
+    if ! command -v haproxy &> /dev/null; then
+        echo "HAProxy is not installed. Installing now."
+        install_haproxy
+    fi
+    add_service_to_haproxy
+else
+    echo "Invalid choice. Exiting."
+    exit 1
+fi
