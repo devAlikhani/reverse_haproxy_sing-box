53c51d3b18ebbcfbb67611d079e5c69dcbc2fbe2
Mohammad Alikhani <dev.alikhani@gmail.com>
2023-11-24 11:39:20 +0330

    One option was added due to adding service to haproxy.

--- install.sh
+++ install.sh
@@ -1,71 +1,53 @@
 #!/bin/bash
 
-# Ask for the domain name
-read -p "Enter the domain to be served by Caddy: " domain
-
 # Function to install Caddy
 install_caddy() {
     echo "Installing Caddy..."
-    apt install -y debian-keyring debian-archive-keyring apt-transport-https
-    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-    apt update
-    apt install caddy
+    apt-get install -y debian-keyring debian-archive-keyring apt-transport-https
+    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/caddy-stable-archive-keyring.gpg] https://dl.cloudsmith.io/public/caddy/stable/deb/debian any-version main" | tee /etc/apt/sources.list.d/caddy-stable.list
+    apt-get update
+    apt-get install caddy
 }
 
 # Function to install HAProxy
 install_haproxy() {
     echo "Installing HAProxy..."
-    sudo apt update
-    sudo apt install haproxy
+    apt-get update
+    apt-get install haproxy
 }
 
-# Check if Caddy is installed and running
-if ! command -v caddy &> /dev/null; then
-    install_caddy
-fi
-
-# Check if HAProxy is installed and running
-if ! command -v haproxy &> /dev/null; then
-    install_haproxy
-fi
-
-# Configure Caddy
-echo "Configuring Caddy for $domain..."
-cat <<EOF | sudo tee /etc/caddy/Caddyfile
-
+# Function to configure Caddy and HAProxy
+configure_caddy_haproxy() {
+    # Configure Caddy
+    echo "Configuring Caddy for $domain..."
+    cat <<EOF | tee /etc/caddy/Caddyfile
 {
     http_port 8080
     https_port 5003
 }
 
-
 $domain {
-    
-handle /sub/* {
-    root * /var/www/textfiles/
-    file_server
-    uri strip_prefix /sub
-}
+    handle /sub/* {
+        root * /var/www/textfiles/
+        file_server
+        uri strip_prefix /sub
+    }
 
-handle /ssh/* {
-    root * /var/ssh-users/
-    file_server
-    uri strip_prefix /ssh
-}
-
-  # You may want to add additional configurations here, 
-  # like logging, error handling, etc.    
-
+    handle /ssh/* {
+        root * /var/ssh-users/
+        file_server
+        uri strip_prefix /ssh
+    }
 }
 EOF
 
-# Restart Caddy to apply changes
-sudo systemctl restart caddy
+    # Restart Caddy to apply changes
+    systemctl restart caddy
 
-# Configure HAProxy
-echo "Configuring HAProxy..."
-cat <<EOF | sudo tee /etc/haproxy/haproxy.cfg
+    # Configure HAProxy
+    echo "Configuring HAProxy..."
+    cat <<EOF | tee /etc/haproxy/haproxy.cfg
 global
     log /dev/log local0
     log /dev/log local1 notice
@@ -90,18 +72,56 @@ frontend https_in
     tcp-request inspect-delay 5s
     tcp-request content accept if { req_ssl_hello_type 1 }
     use_backend caddy_backend if { req_ssl_sni -i $domain }
-    use_backend sing-box if { req_ssl_sni -i www.speedtest.net }
 
 backend caddy_backend
     mode tcp
     server caddy_server 127.0.0.1:5003 check
-
-backend sing-box
-    mode tcp
-    server localhost 127.0.0.1:5002 check
 EOF
 
-# Restart HAProxy to apply changes
-sudo systemctl restart haproxy
+    # Restart HAProxy to apply changes
+    systemctl restart haproxy
+}
 
-echo "Configuration completed."
+# Function to add a new service to HAProxy
+add_service_to_haproxy() {
+    read -p "Enter the port for the new service (e.g., 5002): " service_port
+    read -p "Enter the SNI domain to route to the new service (e.g., service.example.com): " service_sni
+
+    # Add new service configuration to HAProxy
+    echo "Adding new service to HAProxy..."
+    echo "
+backend new_service_backend
+    mode tcp
+    server new_service 127.0.0.1:$service_port check" | tee -a /etc/haproxy/haproxy.cfg
+
+    # Modify the frontend to add a rule for the new service
+    sed -i "/frontend https_in/a \    use_backend new_service_backend if { req_ssl_sni -i $service_sni }" /etc/haproxy/haproxy.cfg
+
+    # Restart HAProxy to apply changes
+    systemctl restart haproxy
+
+    echo "New service added to HAProxy."
+}
+
+# Main script starts here
+echo "1. Install and configure Caddy and HAProxy from scratch."
+echo "2. Add a new service to an existing HAProxy setup."
+read -p "Choose an option (1 or 2): " choice
+
+if [ "$choice" == "1" ]; then
+    read -p "Enter the domain to be served by Caddy: " domain
+    if ! command -v caddy &> /dev/null || ! command -v haproxy &> /dev/null; then
+        install_caddy
+        install_haproxy
+    fi
+    configure_caddy_haproxy
+elif [ "$choice" == "2" ]; then
+    if ! command -v haproxy &> /dev/null; then
+        echo "HAProxy is not installed. Installing now."
+        install_haproxy
+    fi
+    add_service_to_haproxy
+else
+    echo "Invalid choice. Exiting."
+    exit 1
+fi
